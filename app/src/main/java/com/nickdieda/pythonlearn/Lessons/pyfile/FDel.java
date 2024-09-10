96611d51a50ccd1ce423ba4a78596eb03d8a1216
nick <nickeagle888@gmail.com>
2024-09-10 19:24:51 +0300

    compiler 96% project 96% ui 86% lessons 87% home 87%

--- app/src/main/java/com/nickdieda/pythonlearn/Lessons/pyfile/FDel.java
+++ app/src/main/java/com/nickdieda/pythonlearn/Lessons/pyfile/FDel.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.nickdieda.pythonlearn.R;
 
-public class RFile extends AppCompatActivity {
+public class FDel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_rfile);
+        setContentView(R.layout.activity_fdel);
 
 
 
